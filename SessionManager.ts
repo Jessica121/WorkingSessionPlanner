084306ca58e3a33afe71ceaeaa03451fc8e4a516
Rongrong Wang <rowa@microsoft.com>
2021-02-06 15:20:30 -0800

    Added shift if necessary for meetings and more add / remove meetings in the sessions class.

--- SessionManager.ts
+++ SessionManager.ts
@@ -5,52 +5,130 @@ export class SessionManager {
     meetingSessions = new Array<Session>();
     expectedSessionDuration: number;
     maxDurationOverflow = 0;
+    lastSessionIndex = -1;
 
     constructor(expectedSessDuration: number, maxDurationOverflow: number) {
         this.expectedSessionDuration = expectedSessDuration;
         this.maxDurationOverflow = maxDurationOverflow;
     }
 
-    public addMeeting(meeting: Meeting) {
-        if (this.meetingSessions.length == 0) {
-            let sessionId = Math.random();
-            let date = new Date();
-            let session = new Session(sessionId, date, this.expectedSessionDuration, this.maxDurationOverflow);
-            this.meetingSessions.push(session);
+    public addMeetingToTheLastSession(meeting: Meeting, createNewSession = false): boolean {
+        if (this.lastSessionIndex == -1 || createNewSession) {
+            this.generateNewSession();
         }
-        let lastIndex = this.meetingSessions.length - 1;
-        if (lastIndex < 0) {
+
+        if (this.lastSessionIndex < 0) {
             console.error("adding meeting to a nil session");
-        } else {
-            this.meetingSessions[lastIndex].addMeeting(meeting);
+            return false;
         }
+
+        let lastSession = this.meetingSessions[this.lastSessionIndex];
+        let meetingFitFirstAttempt = lastSession.appendMeeting(meeting);
+
+        // Too long meeting prohibited: If the session is empty and the meeting cannot fit, stop right there cuz its never gonna fit.
+        if (!meetingFitFirstAttempt && lastSession.meetingCount() == 0) {
+            console.error("Add meeting: meeting is too big to fit into any session, meeting info: ", meeting);
+            return false;
+        }
+
+        // Try to create a new empty session and add meeting to there
+        this.generateNewSession();
+        let meetingFitSecondAttempt = this.meetingSessions[this.lastSessionIndex].appendMeeting(meeting);
+        
+        if (!meetingFitSecondAttempt) {
+            console.error("Adding meeting to an empty session failed. The meeting length should be within reasonable value. This is not expected. Meeting details: ", meeting);
+            return false;
+        }
+
+        return true;
+    }
+
+    // Force adding a meeting to the top of the session and shift the rest.
+    public addMeetingToSessionByIndex(index: number, meeting: Meeting) {
+        if(this.sessionIdexOutOfBounds(index)) {
+            console.error("Add meeting to session index out of bounds, index: ", index);
+        }
+        this.meetingSessions[index].forcePrependMeeting(meeting);
+        this.shiftIfNecessaryFromSessionIndex(index);
+    }
+
+    // TODO add meeting into previous session if has enough space.
+
+    private generateNewSession() {
+        let sessionId = Math.random();
+        let date = new Date();
+        let session = new Session(sessionId, date, this.expectedSessionDuration, this.maxDurationOverflow);
+        this.meetingSessions.push(session);
+        this.lastSessionIndex = this.meetingSessions.length - 1;
     }
 
     public removeMeetingBy(sessionId: number, meetingId: number) {
-        for (let session of this.meetingSessions) {
+        for (let i = 0; i < this.meetingSessions.length; i++) {
+            let session = this.meetingSessions[i];
             if (session.getSessionId() == sessionId) {
                 session.removeMeetingById(meetingId);
+                this.shiftIfNecessaryFromSessionIndex(i);
                 return;
             }
         }
 
         console.error("Remove meeting by session Id: no such session exists with id: ", sessionId);
-
-        // TODO: Shift if necessary
     }
 
     public editMeetingBy(sessionId: number, meetingId: number, newMeeting: Meeting) {
-        for (let session of this.meetingSessions) {
+        for (let i = 0; i < this.meetingSessions.length; i++) {
+            let session = this.meetingSessions[i];
             if (session.getSessionId() == sessionId) {
-                session.editMeetingById(meetingId, newMeeting);
+                let editSucceed = session.editMeetingById(meetingId, newMeeting);
+                if (!editSucceed) {
+                    if (i == this.lastSessionIndex) {
+                        this.addMeetingToTheLastSession(newMeeting, true);
+                     } else {
+                        this.addMeetingToSessionByIndex(i + 1, newMeeting);
+                     }
+                }
+                this.shiftIfNecessaryFromSessionIndex(i);
                 return;
             }
         }
 
         console.error("Edit meeting by session Id: no such session exists with id: ", sessionId);
-
-        // TODO: Shift if necessary
     }
 
-    
+    public shiftIfNecessaryFromSessionIndex(index: number) {
+        // Recursion stops when the index is out of bounds.
+        if (this.sessionIdexOutOfBounds(index)) {
+            return;
+        }
+
+        let nextIndex = index + 1;
+        // if its already the last session, check for capacity and add a new session if not fit.
+        if (index == this.lastSessionIndex) {
+            let lastSession = this.meetingSessions[this.lastSessionIndex];
+            while (lastSession.exceedsCapacity()) {
+                let lastMeeting = lastSession.removeLastMeeting()
+                this.addMeetingToTheLastSession(lastMeeting);
+            }
+        } else {
+            // If the shifting point is at the middle, then two cases, 
+            // 1. this session has extra space to shift meetings from the next session,
+            // 2. this session exceeds capacity and need to shift its load onto the next session.
+            let curSession = this.meetingSessions[index];
+            let nextSession = this.meetingSessions[index + 1];
+            while(!curSession.exceedsCapacity()) {
+                curSession.appendMeeting(nextSession.removeFirstMeeting());
+            }
+
+            while(curSession.exceedsCapacity()) {
+                nextSession.forcePrependMeeting(curSession.removeLastMeeting());
+            }
+        }
+        
+        // Recursively shift next session.
+        this.shiftIfNecessaryFromSessionIndex(nextIndex);
+    }
+
+    private sessionIdexOutOfBounds(index: number): boolean {
+        return index < 0 || index > this.lastSessionIndex
+    }
 }
--- Sessions.ts
+++ Sessions.ts
@@ -5,6 +5,7 @@ export class Session {
     protected hasHappened = false;
     protected sessionId: number;
     protected dateTime: Date;
+    // Todo meeting interval
     protected currentDuration: number;
     protected expectedDuration: number;
     protected maxDurationOverflow: number;
@@ -20,20 +21,40 @@ export class Session {
         this.hasHappened = hasHappened;
     }
 
+    public exceedsCapacity(meeting: Meeting = null): boolean {
+        let meetingDuration = meeting == null ? 0 : meeting.getDuration();
+        return this.currentDuration + meetingDuration <= this.expectedDuration + this.maxDurationOverflow;
+    }
+
     /**
      * addMeeting
      * add to meeting if there is enough space
      * aka. the current duration of meetings plus this meeting's duration does not exceed the expected duration plus the max overflow time 
      * if so, return true, else return false. 
      */
-    public addMeeting(meeting: Meeting): boolean {
-        if (this.currentDuration + meeting.getDuration() > this.expectedDuration + this.maxDurationOverflow) {
-            // Session manager should be able to manage this. for e.g. add this meeting to the next queue, etc.
+    public appendMeeting(meeting: Meeting): boolean {
+        // If this session cannot fit this meeting, return false, and let session manager deal with shifting.
+        if (!this.exceedsCapacity(meeting)) {
             return false;
         }
-        this.meetings.enqueue(meeting);
-        this.currentDuration += meeting.getDuration();
-        return true;
+
+        let succeed = this.meetings.append(meeting);
+        if (succeed) {
+            this.currentDuration += meeting.getDuration();
+            return true;
+        }
+
+        console.error("Append meeting append failed, meeting info: ", meeting);
+        
+        return false;
+    }
+
+    // Add meeting to head regardlessly, don't check for fitness and let the manager deal with shifting.
+    public forcePrependMeeting(meeting: Meeting) {
+        let succeed = this.meetings.prepend(meeting);
+        if (!succeed) {
+            console.error("forcePrependMeeting failed, meeting info: ", meeting);
+        }
     }
 
     /**
@@ -43,6 +64,7 @@ export class Session {
         for (let meeting of this.meetings) {
             if (meeting.getMeetingId() == id) {
                 this.meetings.remove(meeting);
+                this.currentDuration -= meeting.getDuration();
                 return;
             }
         }
@@ -52,26 +74,56 @@ export class Session {
     /**
      * removeLastMeeting
      */
-    public removeLastMeeting() {
+    public removeLastMeeting(): Meeting {
         if (this.meetings.length > 0) {
-            this.meetings.dequeue();
+            let lastMtg = this.meetings.dequeue();
+            this.currentDuration -= lastMtg.getDuration();
+            return lastMtg;
         } else {
             throw console.error("Cannot remove meetings from an empty session queue.");
         }
     }
 
-    public editMeetingById(id: number, newMeeting: Meeting) {
+    /**
+     * removeFirstMeeting
+     */
+    public removeFirstMeeting(): Meeting {
+        if (this.meetings.length > 0) {
+            let firstMtg = this.meetings.removeHead();
+            this.currentDuration -= firstMtg.getDuration();
+            return firstMtg;
+        } else {
+            throw console.error("Cannot remove meetings from an empty session queue.");
+        }
+    }
+
+    public editMeetingById(id: number, newMeeting: Meeting): boolean {
         for (let meeting of this.meetings) {
             if (meeting.getMeetingId() == id) {
-                this.meetings.insert(newMeeting, meeting, true);
-                return;
+                let editInQueueSucceed = this.meetings.insert(newMeeting, meeting, true);
+                if (editInQueueSucceed) {
+                    this.currentDuration -= meeting.getDuration();
+                    if (!this.exceedsCapacity(newMeeting)) {
+                        this.meetings.remove(newMeeting);
+                        console.error("Edit meeting, the new meeting cannot fit into this session: ", newMeeting);
+                        return false;
+                    }
+                    this.currentDuration += newMeeting.getDuration();
+                    return true;
+                }
             }
         }
+
         console.error("Edit meeting by id: such meeting not found with id: ", id);
+        return false;
     }
 
     public getMeetings() {
         return this.meetings;
+    }
+
+    public meetingCount() {
+        return this.meetings.length;
     }
 
     public getHasHappened() {
